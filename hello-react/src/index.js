63c660713c0fbcdf9006cc6800377be40e6a1d1e
doej96 <doej96@naver.com>
2021-03-03 13:24:10 +0900

    ....

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
